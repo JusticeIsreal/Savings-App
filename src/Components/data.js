ec7b71b7713a3892180ba2c471f88911226d3ed5
Justice Isreal <justiceyba@gmail.com>
2022-09-28 11:58:15 +0100

    table page rearrangement

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,7 +1,7 @@
 const cardDetails = [
   {
     id: 1,
-    savingsType: "House Rent",
+    savingsType: "Rent",
     cardImg:
       "https://res.cloudinary.com/isreal/image/upload/v1663412296/banking%20app/gold-credit-card-template-isolated-white-background-d-illustration_118047-4728-removebg-preview_bec4gu.png",
     bankName:
@@ -9,13 +9,13 @@ const cardDetails = [
     amount: 17,
     target: 200,
     cardNumber: "2342 5421 4563 2345",
-    expiration: "12/24",
+    expiration: "10/22",
     cardHolderName: "JUSTICE AGBOMA",
     backgroundColor: "rgba(54, 162, 235, 0.2)",
   },
   {
     id: 2,
-    savingsType: "School Fee",
+    savingsType: "Fees",
     cardImg:
       "https://res.cloudinary.com/isreal/image/upload/v1663412238/banking%20app/istockphoto-532251550-612x612-removebg-preview_s4625r.png",
     bankName:
@@ -23,13 +23,13 @@ const cardDetails = [
     amount: 60,
     target: 500,
     cardNumber: "3424 5432 2356 2134",
-    expiration: "12/24",
+    expiration: "01/23",
     cardHolderName: "JUSTICE AGBOMA",
     backgroundColor: "rgba(255, 99, 132, 0.2)",
   },
   {
     id: 3,
-    savingsType: "Vacation",
+    savingsType: "Car",
     cardImg:
       "https://res.cloudinary.com/isreal/image/upload/v1663412166/banking%20app/360_F_343415276_wlAkB3R5BSzsBPiyqXwQrxX5e9dJgaaT-removebg-preview_c4hccp.png",
     bankName:
@@ -37,7 +37,7 @@ const cardDetails = [
     amount: 50,
     target: 150,
     cardNumber: "1234 6543 7890 2345",
-    expiration: "12/24",
+    expiration: "12/22",
     cardHolderName: "JUSTICE AGBOMA",
     backgroundColor: "rgba(255, 159, 64, 0.2)",
   },
@@ -51,13 +51,13 @@ const cardDetails = [
     amount: 25,
     target: 200,
     cardNumber: "2342 5421 4563 2345",
-    expiration: "12/24",
+    expiration: "11/22",
     cardHolderName: "JUSTICE AGBOMA",
     backgroundColor: "rgba(255, 205, 86, 0.2)",
   },
   {
     id: 5,
-    savingsType: "Parents upkeep",
+    savingsType: "Upkeep",
     cardImg:
       "https://res.cloudinary.com/isreal/image/upload/v1663412238/banking%20app/istockphoto-532251550-612x612-removebg-preview_s4625r.png",
     bankName:
@@ -65,7 +65,7 @@ const cardDetails = [
     amount: 40,
     target: 100,
     cardNumber: "3424 5432 2356 2134",
-    expiration: "12/24",
+    expiration: "12/22",
     cardHolderName: "JUSTICE AGBOMA",
     backgroundColor: "rgba(75, 192, 192, 0.2)",
   },
