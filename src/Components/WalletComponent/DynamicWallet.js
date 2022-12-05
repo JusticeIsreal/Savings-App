55d7ba9f164f74c3e8e5141502b78f9724fb19ad
Justice Isreal <justiceyba@gmail.com>
2022-12-05 02:11:28 +0100

    withdraw and delete walet button

--- src/Components/WalletComponent/DynamicWallet.js
+++ src/Components/WalletComponent/DynamicWallet.js
@@ -43,28 +43,35 @@ function DynamicWallet() {
       </button>
       <h3 className="dynamic-waller-header">Here is your Wallet details</h3>
       <div className="dynamic-wallet">
-        <div className="main-card">
-          <img src={fullDetails.logo} alt="" className="logo" />
-          <h3 className="walletName">{fullDetails.walletName}</h3>
-          <p className="card-status">
-            {fullDetails.cardStatus !== "Active"
-              ? "On-hold"
-              : fullDetails.cardStatus}
-            <MdRadioButtonChecked
-              className={`${
-                fullDetails.cardStatus === "Active" ? "active" : "on-hold"
-              }`}
-            />
-            <FcCancel
-              className={`${
-                fullDetails.cardStatus === "Active" ? "on-hold" : "active"
-              }`}
-            />
-          </p>
-          <h1 className="card-amount">₦{fullDetails.amount}</h1>
-          <p className="card-expiration">{fullDetails.expirationDate}</p>
-          <h5 className="card-cardHolder">{fullDetails.cardHolderName}</h5>
+        <div className="dynamic-con">
+          <div className="main-card">
+            <img src={fullDetails.logo} alt="" className="logo" />
+            <h3 className="walletName">{fullDetails.walletName}</h3>
+            <p className="card-status">
+              {fullDetails.cardStatus !== "Active"
+                ? "On-hold"
+                : fullDetails.cardStatus}
+              <MdRadioButtonChecked
+                className={`${
+                  fullDetails.cardStatus === "Active" ? "active" : "on-hold"
+                }`}
+              />
+              <FcCancel
+                className={`${
+                  fullDetails.cardStatus === "Active" ? "on-hold" : "active"
+                }`}
+              />
+            </p>
+            <h1 className="card-amount">₦{fullDetails.amount}</h1>
+            <p className="card-expiration">{fullDetails.expirationDate}</p>
+            <h5 className="card-cardHolder">{fullDetails.cardHolderName}</h5>
+          </div>
+          <div className="withdraw-delete-con">
+            <button disabled>WITHDRAW</button>
+            <button>DELETE WALLET</button>
+          </div>
         </div>
+
         <div className="dynamic-card-details-con">
           <div className="dynamic-card-detail">
             <div>
